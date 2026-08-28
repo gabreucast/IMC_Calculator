0aa95216fa2f0a9a58247ea95e259d6264609484
Gabriel <gabreucast@gmail.com>
2026-08-27 22:00:35 -0300

    male and female cars, changing color on click

--- lib/components/gender_selector.dart
+++ lib/components/gender_selector.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
 
 class GenderSelector extends StatefulWidget {
   const GenderSelector({super.key});
@@ -10,26 +12,77 @@ class GenderSelector extends StatefulWidget {
 class _GenderSelectorState extends State<GenderSelector> {
   String? selectedGender;
 
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         //hombre
-        Column(
-          children: [
-
-          ],
+        Expanded(
+          child: GestureDetector(
+            onTap: () {setState(() {
+              selectedGender = "Hombre";
+            });},
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16, right: 8, left: 16),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: selectedGender == "Hombre"
+                      ? AppColors.backgroundComponentSelected
+                      : AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset("assets/images/male.png", height: 100),
+                          SizedBox(height: 10),
+                          Text("Hombre".toUpperCase(), style: TextStyles.bodyText),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
-
         //mujer
-        Column(
-          children: [
-
-          ],
-        )
-      ]
-
+        Expanded(
+          child: GestureDetector(
+            onTap: (){setState(() {
+              selectedGender = "Mujer";
+            });},
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16, right: 16, left: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: selectedGender == "Mujer"
+                      ? AppColors.backgroundComponentSelected
+                      : AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset("assets/images/female.png", height: 100),
+                          SizedBox(height: 10),
+                          Text("Mujer".toUpperCase(), style: TextStyles.bodyText),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
